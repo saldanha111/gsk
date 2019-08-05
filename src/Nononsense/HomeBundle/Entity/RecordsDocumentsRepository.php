3f93486d94dd190c9e6be953812f556a085ce1ed
Sergio <sergio.saldana@nodalblock.com>
2019-08-05 17:09:50 +0200

    Fix errors

--- src/Nononsense/HomeBundle/Entity/RecordsDocumentsRepository.php
+++ src/Nononsense/HomeBundle/Entity/RecordsDocumentsRepository.php
@@ -38,7 +38,7 @@ class RecordsDocumentsRepository extends EntityRepository
             ->orderBy('r.id', 'DESC');
 
         if (isset($filters["pending_for_me"])) {
-            $list->andWhere('(r.status=1 AND r.usercreatedid=:user_id) OR (r.status=2 AND (s.userid=:user_id OR s.groupid IN (":groups")))');
+            $list->andWhere('(r.status=1 AND r.usercreatedid=:user_id) OR (r.status=2 AND (s.userid=:user_id OR s.groupid IN (:groups)))');
             $list->setParameter('user_id', $user->getId());
             $list->setParameter('groups', $groups);
         }
