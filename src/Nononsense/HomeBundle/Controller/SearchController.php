ccc994613d92eabd7a644302a3599e1c2c8c4dc2
Sergio <sergio.saldana@nodalblock.com>
2019-09-06 12:20:15 +0200

    Finish search2_0

--- src/Nononsense/HomeBundle/Controller/SearchController.php
+++ src/Nononsense/HomeBundle/Controller/SearchController.php
@@ -72,8 +72,8 @@ class SearchController extends Controller
 
         $array_item["suser"]["id"]=$user->getId();
         $array_item["filters"]=$filters;
-        $array_item["items"] = $this->getDoctrine()->getRepository(InstanciasWorkflows::class)->search("list",$filters);
-        $array_item["count"] = $this->getDoctrine()->getRepository(InstanciasWorkflows::class)->search("count",$filters2);
+        $array_item["items"] = $this->getDoctrine()->getRepository(InstanciasSteps::class)->search("list",$filters);
+        $array_item["count"] = $this->getDoctrine()->getRepository(InstanciasSteps::class)->search("count",$filters2);
 
         $url=$this->container->get('router')->generate('nononsense_search');
         $params=$request->query->all();
@@ -106,12 +106,11 @@ class SearchController extends Controller
                  ->setCellValue('G1', 'Num.equipo')
                  ->setCellValue('H1', 'Material')
                  ->setCellValue('I1', 'WO.SAP')
-                 ->setCellValue('J1', 'Estado')
-                 ->setCellValue('K1', 'Reconciliado');
+                 ->setCellValue('J1', 'Estado');
             }
 
             if($request->get("export_pdf")){
-                $html='<html><body style="font-size:8px;width:100%"><table autosize="1" style="overflow:wrap;width:100%"><tr style="font-size:8px;width:100%"><th style="font-size:8px;width:6%">Nº</th><th style="font-size:8px;width:49%">Nombre</th><th style="font-size:8px;width:10%">Iniciado por</th><th style="font-size:8px;width:10%">F. inicio</th><th style="font-size:8px;width:10%">F. modific.</th><th style="font-size:8px;width:10%">Estado</th><th style="font-size:8px;width:5%">Reconc.</th></tr>';
+                $html='<html><body style="font-size:8px;width:100%"><table autosize="1" style="overflow:wrap;width:100%"><tr style="font-size:8px;width:100%"><th style="font-size:8px;width:6%">Nº</th><th style="font-size:8px;width:49%">Nombre</th><th style="font-size:8px;width:10%">Iniciado por</th><th style="font-size:8px;width:10%">F. inicio</th><th style="font-size:8px;width:10%">F. modific.</th><th style="font-size:8px;width:10%">Estado</th></tr>';
             }
 
             $i=2;
@@ -135,11 +134,17 @@ class SearchController extends Controller
                     case 15: $status="Esperando firma verificación parcial";break;
                     default: $status="Desconocido";
                 }
+                if($item["id_grid"]==0){
+                    $name=$item["name"];
+                }
+                else{
+                    $name=$item["name2"];
+                }
 
                 if($request->get("export_excel")){
                     $phpExcelObject->getActiveSheet()
-                    ->setCellValue('A'.$i, $item["id"])
-                    ->setCellValue('B'.$i, $item["name"])
+                    ->setCellValue('A'.$i, $item["id_grid"])
+                    ->setCellValue('B'.$i, $name)
                     ->setCellValue('C'.$i, $item["creator"])
                     ->setCellValue('D'.$i, ($item["created"]) ? $item["created"] : '')
                     ->setCellValue('E'.$i, ($item["modified"]) ? $item["modified"] : '')
@@ -147,212 +152,11 @@ class SearchController extends Controller
                     ->setCellValue('G'.$i, $item["equipo"])
                     ->setCellValue('H'.$i, $item["material"])
                     ->setCellValue('I'.$i, $item["workordersap"])
-                    ->setCellValue('J'.$i, $status)
-                    ->setCellValue('K'.$i, $item["id_reconciliado"]);
-
-                    if($item["checklist"] == 1 && ($item["status"] == 4 || $item["status"] == 7 || $item["status"] == 12 || $item["status"] == 13 || $item["status"] == 15 || $item["status"] == 9 || $item["status"] == 10)){
-                        $i++;
-                        $phpExcelObject->getActiveSheet()
-                            ->setCellValue('A'.$i, $item["id"]."-CH1")
-                            ->setCellValue('B'.$i, $item["chname"])
-                            ->setCellValue('C'.$i, $item["creator"])
-                            ->setCellValue('D'.$i, ($item["created"]) ? $item["created"] : '')
-                            ->setCellValue('E'.$i, ($item["modified"]) ? $item["modified"] : '')
-                            ->setCellValue('F'.$i, $item["lote"])
-                            ->setCellValue('G'.$i, $item["equipo"])
-                            ->setCellValue('H'.$i, $item["material"])
-                            ->setCellValue('I'.$i, $item["workordersap"])
-                            ->setCellValue('J'.$i, $status)
-                            ->setCellValue('K'.$i, '');
-                    }
+                    ->setCellValue('J'.$i, $status);
                 }
 
                 if($request->get("export_pdf")){
-                    $html.='<tr style="font-size:8px"><td>'.$item["id"].'</td><td>'.$item["name"].'</td><td>'.$item["creator"].'</td><td>'.(($item["created"]) ? $item["created"]->format('Y-m-d H:i:s') : '').'</td><td>'.(($item["modified"]) ? $item["modified"]->format('Y-m-d H:i:s') : '').'</td><td>'.$status.'</td><td>'.$item["id_reconciliado"].'</td></tr>';
-                    if($item["checklist"] == 1 && ($item["status"] == 4 || $item["status"] == 7 || $item["status"] == 12 || $item["status"] == 13 || $item["status"] == 15 || $item["status"] == 9 || $item["status"] == 10)){
-                        $i++;
-                        $html.='<tr style="font-size:8px"><td>'.$item["id"].'-CH1</td><td>'.$item["chname"].'</td><td>'.$item["creator"].'</td><td>'.(($item["created"]) ? $item["created"]->format('Y-m-d H:i:s') : '').'</td><td>'.(($item["modified"]) ? $item["modified"]->format('Y-m-d H:i:s') : '').'</td><td>'.$status.'</td><td></td></tr>';
-                    }
-                }
-
-                $i++;
-            }
-
-            if($request->get("export_excel")){
-                $phpExcelObject->getActiveSheet()->setTitle('Listado de registros');
-                // Set active sheet index to the first sheet, so Excel opens this as the first sheet
-                $phpExcelObject->setActiveSheetIndex(0);
-
-                // create the writer
-                $writer = $this->get('phpexcel')->createWriter($phpExcelObject, 'Excel2007');
-                // create the response
-                $response = $this->get('phpexcel')->createStreamedResponse($writer);
-                // adding headers
-                $dispositionHeader = $response->headers->makeDisposition(
-                  ResponseHeaderBag::DISPOSITION_ATTACHMENT,
-                  'list_records.xlsx'
-                );
-                $response->headers->set('Content-Type', 'text/vnd.ms-excel; charset=utf-8');
-                $response->headers->set('Pragma', 'public');
-                $response->headers->set('Cache-Control', 'maxage=1');
-                $response->headers->set('Content-Disposition', $dispositionHeader);
-
-                return $response; 
-            }
-
-            if($request->get("export_pdf")){
-                $html.='</table></body></html>';
-                $this->returnPDFResponseFromHTML($html);
-            }
-        }
-    }
-
-    public function list2Action(Request $request){
-
-        $user = $this->container->get('security.context')->getToken()->getUser();
-        $fll=false;
-        foreach ($user->getGroups() as $groupMe) {
-            $type = $groupMe->getGroup()->getTipo();
-            if ($type == 'FLL') {
-                $fll = true;
-            }
-        }
-
-        $array_item["fll"]=$fll;
-
-        $filters=Array();
-        $filters2=Array();
-        $types=array();
-
-        $filters=array_filter($request->query->all());
-        $filters2=array_filter($request->query->all());
-
-        $filters["user"]=$user;
-        $filters2["user"]=$user;
-
-        $filters["fll"]=$fll;
-        $filters2["fll"]=$fll;
-
-        $array_item["suser"]["id"]=$user->getId();
-
-        if(!$request->get("export_excel") && !$request->get("export_pdf")){
-            if($request->get("page")){
-                $filters["limit_from"]=$request->get("page")-1;
-            }
-            else{
-                $filters["limit_from"]=0;
-            }
-            $filters["limit_many"]=15;
-        }
-        else{
-            $filters["limit_from"]=0;
-            $filters["limit_many"]=99999999999;
-        }
-
-
-        $array_item["suser"]["id"]=$user->getId();
-        $array_item["filters"]=$filters;
-        $array_item["items"] = $this->getDoctrine()->getRepository(InstanciasSteps::class)->search("list",$filters);
-        $array_item["count"] = $this->getDoctrine()->getRepository(InstanciasSteps::class)->search("count",$filters2);
-
-        $url=$this->container->get('router')->generate('nononsense_search2');
-        $params=$request->query->all();
-        unset($params["page"]);
-        if(!empty($params)){
-            $parameters=TRUE;
-        }
-        else{
-            $parameters=FALSE;
-        }
-        $array_item["pagination"]=\Nononsense\UtilsBundle\Classes\Utils::paginador($filters["limit_many"],$request,$url,$array_item["count"],"/", $parameters);
-
-        if(!$request->get("export_excel") && !$request->get("export_pdf")){
-            return $this->render('NononsenseHomeBundle:Contratos:search2.html.twig',$array_item);
-        }
-        else{
-            //Exportamos a Excel
-
-            if($request->get("export_excel")){
-                $phpExcelObject = $this->get('phpexcel')->createPHPExcelObject();
-
-                $phpExcelObject->getProperties();
-                $phpExcelObject->setActiveSheetIndex(0)
-                 ->setCellValue('A1', 'Nº')
-                 ->setCellValue('B1', 'Nombre')
-                 ->setCellValue('C1', 'Iniciado por')
-                 ->setCellValue('D1', 'Fecha inicio')
-                 ->setCellValue('E1', 'Ultima modificación')
-                 ->setCellValue('F1', 'Lote')
-                 ->setCellValue('G1', 'Num.equipo')
-                 ->setCellValue('H1', 'Material')
-                 ->setCellValue('I1', 'WO.SAP')
-                 ->setCellValue('J1', 'Estado')
-                 ->setCellValue('K1', 'Reconciliado');
-            }
-
-            if($request->get("export_pdf")){
-                $html='<html><body style="font-size:8px;width:100%"><table autosize="1" style="overflow:wrap;width:100%"><tr style="font-size:8px;width:100%"><th style="font-size:8px;width:6%">Nº</th><th style="font-size:8px;width:49%">Nombre</th><th style="font-size:8px;width:10%">Iniciado por</th><th style="font-size:8px;width:10%">F. inicio</th><th style="font-size:8px;width:10%">F. modific.</th><th style="font-size:8px;width:10%">Estado</th><th style="font-size:8px;width:5%">Reconc.</th></tr>';
-            }
-
-            $i=2;
-            foreach($array_item["items"] as $item){
-                switch($item["status"]){
-                    case 0: $status="Iniciado";break;
-                    case 1: $status="Esperando firma guardado parcial";break;
-                    case 2: $status="Esperando firma envío";break;
-                    case 3: $status="Esperando firma cancelación";break;
-                    case 4: $status="En verificación";break;
-                    case 5: $status="Pendiente cancelación en edición";break;
-                    case 6: $status="Cancelado en edición";break;
-                    case 7: $status="Esperando firma verificación total";break;
-                    case 8: $status="Cancelado";break;
-                    case 9: $status="Archivado";break;
-                    case 10: $status="Reconciliado";break;
-                    case 11: $status="Bloqueado";break;
-                    case 12: $status="Esperando firma cancelación en verificación";break;
-                    case 13: $status="Esperando firma devolución a edición";break;
-                    case 14: $status="Pendiente de cancelación en verificación";break;
-                    case 15: $status="Esperando firma verificación parcial";break;
-                    default: $status="Desconocido";
-                }
-
-                if($request->get("export_excel")){
-                    $phpExcelObject->getActiveSheet()
-                    ->setCellValue('A'.$i, $item["id"])
-                    ->setCellValue('B'.$i, $item["name"])
-                    ->setCellValue('C'.$i, $item["creator"])
-                    ->setCellValue('D'.$i, ($item["created"]) ? $item["created"] : '')
-                    ->setCellValue('E'.$i, ($item["modified"]) ? $item["modified"] : '')
-                    ->setCellValue('F'.$i, $item["lote"])
-                    ->setCellValue('G'.$i, $item["equipo"])
-                    ->setCellValue('H'.$i, $item["material"])
-                    ->setCellValue('I'.$i, $item["workordersap"])
-                    ->setCellValue('J'.$i, $status)
-                    ->setCellValue('K'.$i, $item["id_reconciliado"]);
-
-                    if($item["checklist"] == 1 && ($item["status"] == 4 || $item["status"] == 7 || $item["status"] == 12 || $item["status"] == 13 || $item["status"] == 15 || $item["status"] == 9 || $item["status"] == 10)){
-                        $i++;
-                        $phpExcelObject->getActiveSheet()
-                            ->setCellValue('A'.$i, $item["id"]."-CH1")
-                            ->setCellValue('B'.$i, $item["chname"])
-                            ->setCellValue('C'.$i, $item["creator"])
-                            ->setCellValue('D'.$i, ($item["created"]) ? $item["created"] : '')
-                            ->setCellValue('E'.$i, ($item["modified"]) ? $item["modified"] : '')
-                            ->setCellValue('F'.$i, $item["lote"])
-                            ->setCellValue('G'.$i, $item["equipo"])
-                            ->setCellValue('H'.$i, $item["material"])
-                            ->setCellValue('I'.$i, $item["workordersap"])
-                            ->setCellValue('J'.$i, $status)
-                            ->setCellValue('K'.$i, '');
-                    }
-                }
-
-                if($request->get("export_pdf")){
-                    $html.='<tr style="font-size:8px"><td>'.$item["id"].'</td><td>'.$item["name"].'</td><td>'.$item["creator"].'</td><td>'.(($item["created"]) ? $item["created"]->format('Y-m-d H:i:s') : '').'</td><td>'.(($item["modified"]) ? $item["modified"]->format('Y-m-d H:i:s') : '').'</td><td>'.$status.'</td><td>'.$item["id_reconciliado"].'</td></tr>';
-                    if($item["checklist"] == 1 && ($item["status"] == 4 || $item["status"] == 7 || $item["status"] == 12 || $item["status"] == 13 || $item["status"] == 15 || $item["status"] == 9 || $item["status"] == 10)){
-                        $i++;
-                        $html.='<tr style="font-size:8px"><td>'.$item["id"].'-CH1</td><td>'.$item["chname"].'</td><td>'.$item["creator"].'</td><td>'.(($item["created"]) ? $item["created"]->format('Y-m-d H:i:s') : '').'</td><td>'.(($item["modified"]) ? $item["modified"]->format('Y-m-d H:i:s') : '').'</td><td>'.$status.'</td><td></td></tr>';
-                    }
+                    $html.='<tr style="font-size:8px"><td>'.$item["id"].'</td><td>'.$name.'</td><td>'.$item["creator"].'</td><td>'.(($item["created"]) ? $item["created"]->format('Y-m-d H:i:s') : '').'</td><td>'.(($item["modified"]) ? $item["modified"]->format('Y-m-d H:i:s') : '').'</td><td>'.$status.'</td></tr>';
                 }
 
                 $i++;
