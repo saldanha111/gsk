a474f21eebe3794aa7bb7da9d55ef938128b0d2c
Manuel Ufarte <mufarte@oaro.net>
2020-07-24 19:03:27 +0200

    GSK-11-cada-miembro-del-comité-recibe-un Cambios para enviar al comité el contrato por SMS

--- web/js/js_oarodoc/contracts_sign_comite_rrhh.js
+++ web/js/js_oarodoc/contracts_sign_comite_rrhh.js
@@ -3,6 +3,7 @@ $( document ).ready(function() {
 	$("#btn_save_partial").hide();
 
 	//las variables que se deban ocultar en la vista del comite deben tener un name que empiece por 'dat_tra_   y con la siguiente linea las oculto
-	$(":input[name^='dat_tra_']").css("display","none");
-
+	$( ":input[name^='dat_tra_']" ).each(function( index, element ) {
+		element.parentNode.removeChild(element);
+	});
 });
