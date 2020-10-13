fb2fb027dae9843002a74893a6289a820e1bf773
SALDAPC\salda <salen222@gmail.com>
2020-10-13 16:59:34 +0200

    Seguimos con la verificación

--- src/Nononsense/HomeBundle/Controller/TemplateTestTemplatesController.php
+++ src/Nononsense/HomeBundle/Controller/TemplateTestTemplatesController.php
@@ -18,6 +18,8 @@ use Nononsense\HomeBundle\Entity\TMSignatures;
 use Nononsense\HomeBundle\Entity\TMWorkflow;
 use Nononsense\HomeBundle\Entity\TMCumplimentations;
 use Nononsense\HomeBundle\Entity\TMSecondWorkflow;
+use Nononsense\HomeBundle\Entity\TMTests;
+use Nononsense\HomeBundle\Entity\TMTestResults;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -51,6 +53,7 @@ class TemplateTestTemplatesController extends Controller
 
         $action = $this->getDoctrine()->getRepository(TMActions::class)->findOneBy(array("id" => 3));
         $testers = $this->getDoctrine()->getRepository(TMWorkflow::class)->findBy(array("template" => $array_item["template"], "action" => $action),array("id" => "ASC"));
+
         $find=0;
         foreach($testers as $tester){
             if($tester->getUserEntiy() && $tester->getUserEntiy()==$user){
@@ -85,6 +88,16 @@ class TemplateTestTemplatesController extends Controller
             return $this->redirect($route);
         }
 
+        if($request->get("token")){
+        	$test = $this->getDoctrine()->getRepository(TMTests::class)->findOneBy(array("token" => $request->get("token"), "signature" => NULL, "userEntiy" => $user));
+        	if($test){
+        		$array_item["sign"]=1;
+        	}
+        }
+
+        $signatures = $this->getDoctrine()->getRepository(TMSignatures::class)->findBy(array("template" => $array_item["template"], "action" => $action),array("id" => "ASC"));
+        $array_item["tests"] = $this->getDoctrine()->getRepository(TMTests::class)->findBy(array("signature" => $signatures, "test_id" => NULL),array("id" => "DESC"));
+
         $base_url=$this->getParameter('api_docoaro')."/documents/".$array_item["template"]->getPlantillaId();
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $base_url);
@@ -111,6 +124,7 @@ class TemplateTestTemplatesController extends Controller
 
         $action = $this->getDoctrine()->getRepository(TMActions::class)->findOneBy(array("id" => 3));
         $array_item["testers"] = $this->getDoctrine()->getRepository(TMWorkflow::class)->findBy(array("template" => $array_item["template"], "action" => $action),array("id" => "ASC"));
+        $array_item["results"] = $this->getDoctrine()->getRepository(TMTestResults::class)->findBy(array(),array("id" => "ASC"));
 
 
         return $this->render('NononsenseHomeBundle:TemplateManagement:test_detail.html.twig',$array_item);
@@ -234,5 +248,204 @@ class TemplateTestTemplatesController extends Controller
             return $responseAction;
 
         }
+    }
+
+    public function updateAction(Request $request, int $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $array_item=array();
+
+        $user = $this->container->get('security.context')->getToken()->getUser();
+
+        $template = $this->getDoctrine()->getRepository(TMTemplates::class)->findOneBy(array("id" => $id));
+        if($template->getTmState()->getId()!=3){
+            $this->get('session')->getFlashBag()->add(
+                'error',
+                'La plantilla indicada no se encuentra en fase Test'
+            );
+            $route=$this->container->get('router')->generate('nononsense_tm_templates');
+            return $this->redirect($route);
+        }
+
+        if(!$template->getOpenedBy() || $template->getOpenedBy()!=$user){
+            $this->get('session')->getFlashBag()->add(
+                'error',
+                'No se puedo efectuar la operación'
+            );
+            $route=$this->container->get('router')->generate('nononsense_tm_templates');
+            return $this->redirect($route);
+        }
+
+        if($request->get("token")){
+        	$last_test = $this->getDoctrine()->getRepository(TMTests::class)->findOneBy(array("token" => $request->get("token")));
+        	if(!$last_test){
+        		$this->get('session')->getFlashBag()->add(
+	                'error',
+	                'No se puedo efectuar la operación'
+	            );
+	            $route=$this->container->get('router')->generate('nononsense_tm_templates');
+	            return $this->redirect($route);
+        	}
+        }
+        else{
+        	$this->get('session')->getFlashBag()->add(
+                'error',
+                'No se puedo efectuar la operación'
+            );
+            $route=$this->container->get('router')->generate('nononsense_tm_templates');
+            return $this->redirect($route);
+        }
+
+        $action = $this->getDoctrine()->getRepository(TMActions::class)->findOneBy(array("id" => 3));
+        
+        if($request->get("finish_tests")){
+        	$testers = $this->getDoctrine()->getRepository(TMWorkflow::class)->findBy(array("template" => $template, "action" => $action),array("id" => "ASC"));
+	        $find=0;
+	        foreach($testers as $tester){
+	            if($tester->getUserEntiy() && $tester->getUserEntiy()==$user){
+	                $tester->setSigned(TRUE);
+	                $em->persist($tester);
+	                $find=1;
+	            }
+	        }
+	        
+	        if($find==0){
+	            foreach($testers as $tester){
+	                if($tester->getGroupEntiy() && !$tester->getSigned()){
+	                    $in_group=0;
+	                    foreach($user->getGroups() as $uniq_group){
+	                        if($uniq_group->getGroup()==$tester->getGroupEntiy()){
+	                            $in_group=1;
+	                            break;
+	                        }
+	                    }
+	                    if($in_group==1){
+	                        $find=1;
+	                        $tester->setSigned(TRUE);
+	                        $em->persist($tester);
+	                        break;
+	                    }
+	                }
+	            }
+	        }
+	    }
+	    else{
+	    	$testers = $this->getDoctrine()->getRepository(TMWorkflow::class)->findBy(array("template" => $template, "action" => $action),array("id" => "DESC"));
+	        $find=0;
+
+        	foreach($testers as $tester){
+                if($tester->getGroupEntiy()){
+                    $in_group=0;
+                    foreach($user->getGroups() as $uniq_group){
+                        if($uniq_group->getGroup()==$tester->getGroupEntiy()){
+                            $in_group=1;
+                            break;
+                        }
+                    }
+                    if($in_group==1){
+                        $find=1;
+                        $tester->setSigned(FALSE);
+                        $em->persist($tester);
+                        break;
+                    }
+                }
+            }
+	        
+	        if($find==0){
+	        	foreach($testers as $tester){
+		            if($tester->getUserEntiy() && $tester->getUserEntiy()==$user){
+		                $tester->setSigned(FALSE);
+		                $em->persist($tester);
+		                $find=1;
+		            }
+		        }
+	        }
+	    }
+
+        if($find==0){
+            $this->get('session')->getFlashBag()->add(
+                'error',
+                'No tiene permisos para testear este documento'
+            );
+            $route=$this->container->get('router')->generate('nononsense_tm_templates');
+            return $this->redirect($route);
+        }
+
+        $base_url=$this->getParameter('api_docoaro')."/documents/".$template->getPlantillaId();
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $base_url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST,"GET");
+        curl_setopt($ch, CURLOPT_HTTPHEADER,array("Api-Key: ".$this->getParameter('api_key_docoaro')));
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, array());    
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $raw_response = curl_exec($ch);
+        $response = json_decode($raw_response, true);
+
+        $signature = new TMSignatures();
+        $signature->setTemplate($template);
+        $signature->setAction($action);
+        $signature->setUserEntiy($user);
+        $signature->setCreated(new \DateTime());
+        $signature->setModified(new \DateTime());
+        $signature->setSignature($request->get("signature"));
+        $signature->setVersion($response["version"]["id"]);
+        $signature->setConfiguration($response["version"]["configuration"]["id"]);
+        if($request->get("finish_tests")){
+        	if($request->get("description")){
+        		$signature->setDescription($request->get("description")." - Finalizo tests");
+        	}
+        	else{
+        		$signature->setDescription("Finalizo tests");
+        	}
+        	
+        }
+        else{
+        	if($request->get("description")){
+        		$signature->setDescription($request->get("description"));
+        	}
+        }
+        $em->persist($signature);
+
+        
+        $result = $this->getDoctrine()->getRepository(TMTestResults::class)->findOneBy(array("id" => $request->get("result")));
+        $last_test->setSignature($signature);
+        $last_test->setResult($result);
+        $em->persist($last_test);
+        
+
+        if($request->get("finish_tests")){
+	        $template->setOpenedBy(NULL);
+    	    $template->setToken(NULL);
+    	
+	        $next_step=1;
+	        foreach($testers as $tester){
+	            if(!$tester->getSigned()){
+	                $next_step=0;
+	            }
+	        }
+
+	        if($next_step==1){
+	        	$next_state=4;
+	            $state = $this->getDoctrine()->getRepository(TMStates::class)->findOneBy(array("id"=> $next_state));
+	            $template->setTmState($state);
+	        }
+
+        }
+
+        $em->persist($template);
+        $em->flush();
+
+       
+        if($request->get("finish_tests")){
+        	$this->get('session')->getFlashBag()->add('message', "La operación se ha ejecutado con éxito");
+        	$route = $this->container->get('router')->generate('nononsense_tm_templates');
+        }
+        else{
+        	 $this->get('session')->getFlashBag()->add('message', "El test se ha añadido correctamente");
+        	$route = $this->container->get('router')->generate('nononsense_tm_test_detail', array("id" => $template->getId()),TRUE);
+        }
+        return $this->redirect($route);
     }
 }
