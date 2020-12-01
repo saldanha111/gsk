3c880c4914341c88d578c985230c14d86160df10
alejandro <alexhamakuko@gmail.com>
2020-12-01 14:51:45 +0100

    Update PendingValidationsCommand.php

--- src/Nononsense/HomeBundle/Command/PendingValidationsCommand.php
+++ src/Nononsense/HomeBundle/Command/PendingValidationsCommand.php
@@ -36,7 +36,7 @@ class PendingValidationsCommand extends ContainerAwareCommand
 	        $baseUrl = trim($this->getContainer()->getParameter('cm_installation'), '/').$this->getContainer()->get('router')->generate('nononsense_search');
 
 	        foreach ($users as $key => $user) {
-	        	if ($this->get('utilities')->sendNotification($user['email'], $baseUrl, "", "", $subject, $message)) {
+	        	if ($this->getContainer()->get('utilities')->sendNotification($user['email'], $baseUrl, "", "", $subject, $message)) {
 	        		
 	        		$output->writeln(['<options=bold>Mensaje enviado:</> '.$user['email']]);
 
