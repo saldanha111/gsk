1e784fe1eacb3df162e0d3ebdc241d3d72067cd8
Manuel Ufarte <mufarte@oaro.net>
2021-02-12 12:56:04 +0100

    FIX check dissolution

---
+++ web/js/js_oarodoc/contracts_without_perms_comite.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+	$("#btn_save").hide();
+	$("#btn_save_partial").hide();
+	// importante que este vaya antes que el display none, si no, no se oculta ninguna
+	$("input.form-control").css('display','inline-block');
+	//las variables que se deban ocultar en la vista del comite deben tener un name que empiece por 'dat_tra_   y con la siguiente linea las oculto
+	$(":input[name^='dat_tra_']").css("display","none");
+	$("p").css('margin-top','5px');
+	$("p").css('margin-bottom','5px');
+	$("td > p").css('margin','0');
+});
