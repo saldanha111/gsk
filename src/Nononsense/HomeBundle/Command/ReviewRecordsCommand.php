100d08d33a13dc6ee90e7cd71aa3c748649b6ff8
SALDAPC\salda <salen222@gmail.com>
2021-11-12 10:45:04 +0100

    Fix error on cron stand by

--- src/Nononsense/HomeBundle/Command/ReviewRecordsCommand.php
+++ src/Nononsense/HomeBundle/Command/ReviewRecordsCommand.php
@@ -46,11 +46,33 @@ class ReviewRecordsCommand extends ContainerAwareCommand
 		    				->getQuery()
 		    				->getResult();
 		   
-		    if ($records) {				
+		    if ($records) {	
+		    	$typesw = $em->getRepository(CVSecondWorkflowStates::class)->findOneBy(array("id" => "2"));
+			    $specific = $em->getRepository(SpecificGroups::class)->findOneBy(array("name" => "ECO"));
+	            $other_group = $specific->getGroup();
+
 			    foreach ($records as $key => $record) {
 		    		$record->setBlocked(1);
 		    		$em->persist($record);
 		    		$ids[] = $record->getId();
+
+	            	$sworkflow = new CVSecondWorkflow();
+		            $sworkflow->setRecord($record);
+		            $sworkflow->setGroup($other_group);
+		            $sworkflow->setNumberSignature(1);
+		            $sworkflow->setType($typesw);
+		            $sworkflow->setSigned(FALSE);
+		            $em->persist($sworkflow);
+
+		            if($area->getFll()){
+		            	$sworkflow = new CVSecondWorkflow();
+			            $sworkflow->setRecord($record);
+			            $sworkflow->setUser($area->getFll());
+			            $sworkflow->setNumberSignature(2);
+			            $sworkflow->setType($typesw);
+			            $sworkflow->setSigned(FALSE);
+			            $em->persist($sworkflow);
+		            }
 			    }
 			}
 
@@ -61,29 +83,11 @@ class ReviewRecordsCommand extends ContainerAwareCommand
 		        $message = 'Los siguientes registros han sido bloqueados y necesitan ser gestionados por su parte o algún otro FLL. Acceda al siguiente  Link para gestionar los bloqueos.<br><br>'.implode('<br>', $ids);
 		        $baseUrl = trim($this->getContainer()->getParameter('cm_installation'), '/').$this->getContainer()->get('router')->generate('nononsense_cv_search')."?blocked=1";
 
-		        $typesw = $em->getRepository(CVSecondWorkflowStates::class)->findOneBy(array("id" => "2"));
-		        $specific = $em->getRepository(SpecificGroups::class)->findOneBy(array("name" => "ECO"));
-            	$other_group = $specific->getGroup();
-
-            	$sworkflow = new CVSecondWorkflow();
-	            $sworkflow->setRecord($record);
-	            $sworkflow->setGroup($other_group);
-	            $sworkflow->setNumberSignature(1);
-	            $sworkflow->setType($typesw);
-	            $sworkflow->setSigned(FALSE);
-	            $em->persist($sworkflow);
+		        
 
 	            if($area->getFll()){
 		            if ($this->getContainer()->get('utilities')->sendNotification($area->getFll()->getEmail(), $baseUrl, "", "", $subject, $message)) {
 
-		            	$sworkflow = new CVSecondWorkflow();
-			            $sworkflow->setRecord($record);
-			            $sworkflow->setUser($area->getFll());
-			            $sworkflow->setNumberSignature(2);
-			            $sworkflow->setType($typesw);
-			            $sworkflow->setSigned(FALSE);
-			            $em->persist($sworkflow);
-		                
 		                $output->writeln(['Mensaje enviado: '.$area->getFll()->getEmail()]);
 
 		                if ($input->getOption('msg')) {
