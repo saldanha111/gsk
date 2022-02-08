d9f6ab91ccf9c420707d150a53f542f0491648e4
SALDAPC\salda <salen222@gmail.com>
2022-02-08 16:09:53 +0100

    Test pdf without images

--- src/Nononsense/HomeBundle/Controller/CVCumplimentationController.php
+++ src/Nononsense/HomeBundle/Controller/CVCumplimentationController.php
@@ -1140,12 +1140,12 @@ class CVCumplimentationController extends Controller
                                 if($value){
                                     if(is_array($value)){
                                         if(array_key_exists("value", $value)){
-                                            $html.='<img src='.$value["value"].' style="width:50px">';
+                                            $html.='IMG';
                                         }
                                         else{
                                             foreach($value as $key2=>$value2){
                                                 if(is_array($value2) && array_key_exists("value", $value2)){
-                                                    $html.='<img src='.$value2["value"].' style="width:50px"> - L'.$key2.'<br>';
+                                                    $html.='IMG - L'.$key2.'<br>';
                                                 }
                                                 else{ 
                                                     $html.=$value2.' - L'.$key2.'<br>';
