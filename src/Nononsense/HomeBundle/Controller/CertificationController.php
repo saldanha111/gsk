fc19bb134b68c41abfa72f6759e1843070ad4d8c
aleon_oaro <aleon@oaro.net>
2022-05-24 12:24:31 +0200

    [wip] fixing issue

--- src/Nononsense/HomeBundle/Controller/CertificationController.php
+++ src/Nononsense/HomeBundle/Controller/CertificationController.php
@@ -258,7 +258,7 @@ class CertificationController extends Controller
                 header('Pragma: public');
                 header('Content-Length: ' . filesize($fileName));
 
-                return readfile($fileName);
+                readfile($fileName);
             }
         }
 
