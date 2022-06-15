ffd1ccfb760340b804a59f1e02357754dc7e9114
aleon_oaro <aleon@oaro.net>
2022-06-15 13:18:41 +0200

    [wip] fixing test issue

--- src/Nononsense/HomeBundle/Controller/TemplateTestTemplatesController.php
+++ src/Nononsense/HomeBundle/Controller/TemplateTestTemplatesController.php
@@ -157,6 +157,7 @@ class TemplateTestTemplatesController extends Controller
 
     public function linkAction(Request $request, int $id)
     {
+        $this->sendEmail("TemplateTestTemplatesController", "inicio linkAction2");
     	$em = $this->getDoctrine()->getManager();
         $array_item=array();
 
