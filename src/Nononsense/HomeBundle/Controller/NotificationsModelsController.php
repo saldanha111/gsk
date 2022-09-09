c27e410834b9d601c1ed97bb7fcc9f5b67518607
Ubuntu <ubuntu@oaro.net>
2022-09-09 13:25:42 +0200

    [wip] checking notifications module

--- src/Nononsense/HomeBundle/Controller/NotificationsModelsController.php
+++ src/Nononsense/HomeBundle/Controller/NotificationsModelsController.php
@@ -204,7 +204,7 @@ class NotificationsModelsController extends Controller
         $notificationModel = new NotificationsModels();
 
         /** @var TMTemplates $tmTemplate */
-        die($data["templateId"]);
+        die(json_encode($data["templateId"]));
         $tmTemplate = $this->getDoctrine()->getRepository(TMTemplates::class)->find($data["templateId"]);
 
         $notificationModel->setTemplateId($tmTemplate);
