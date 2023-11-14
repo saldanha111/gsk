aa4b936afb71f5d1eeac9cb60c0f75caf7feee94
Alejandro <asantos@oaro.net>
2023-11-14 14:44:36 +0100

    Update listener fail user

--- src/Nononsense/UserBundle/Security/Events/ExceptionListenerFail.php
+++ src/Nononsense/UserBundle/Security/Events/ExceptionListenerFail.php
@@ -8,22 +8,31 @@ use Nononsense\HomeBundle\Entity\Logs;
 use Nononsense\HomeBundle\Entity\LogsTypes;
 use Nononsense\UserBundle\Entity\Users;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ExceptionListenerFail
 {
-	function __construct(EntityManager $entityManager, RequestStack $requestStack)
+	function __construct(EntityManager $entityManager, RequestStack $requestStack,  ValidatorInterface $validator)
 	{
 		$this->em = $entityManager;
 		$this->requestStack = $requestStack;
+        $this->validator = $validator;
 	}
 
     public function onAuthenticationFailureLogin(AuthenticationFailureEvent $event)
     {
     	$request = $this->requestStack->getCurrentRequest();
     	$logType = $this->em->getRepository(LogsTypes::class)->findOneBy(['stringId' => 'ACCESS']); //Access Type
-        $user = $this->em->getRepository(Users::class)->findOneBy([
-            'username' => $event->getAuthenticationToken()->getUsername()
-        ]);
+        $username = $event->getAuthenticationToken()->getUsername();
+
+        $isEmail = $this->validator->validate(
+            $username,
+            new Assert\Email()
+        );
+
+        $filter = (0 === count($isEmail)) ? ['email' => $username] : ['username' => $username];
+        $user = $this->em->getRepository(Users::class)->findOneBy($filter);
 
         $log = new Logs();
         $log->setType($logType);
